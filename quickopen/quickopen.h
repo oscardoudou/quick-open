22c2ad495811304b35cb86257b9cebf8aece3357
oscardoudou <oscar9591@icloud.com>
2020-03-10 23:41:11 -0400

    Initial Commit

---
+++ quickopen/quickopen.h
@@ -0,0 +1,19 @@
+//
+//  quickopen.h
+//  quickopen
+//
+//  Created by 张壹弛 on 3/10/20.
+//  Copyright © 2020 oscardoudou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for quickopen.
+FOUNDATION_EXPORT double quickopenVersionNumber;
+
+//! Project version string for quickopen.
+FOUNDATION_EXPORT const unsigned char quickopenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <quickopen/PublicHeader.h>
+
+
