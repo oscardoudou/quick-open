888f20d4d4ddd64a210dfe87564cfee4b0f49804
oscardoudou <oscar9591@icloud.com>
2020-12-18 00:22:14 -0500

    add template delegate

---
+++ quickopen/QuickOpenDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  QuickOpenDelegate.swift
+//  quickopen
+//
+//  Created by 张壹弛 on 12/12/20.
+//  Copyright © 2020 oscardoudou. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+public protocol QuickOpenDelegate {
+    func recordWasSelected(selected record: Any)
+    func textWasEntered(toBeSearched text: String) -> [Any]
+    /// return row for outlineview
+    func quickOpen(_ record: Any) -> NSView?
+}
